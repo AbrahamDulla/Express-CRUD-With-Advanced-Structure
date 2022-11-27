0208c01bbcbc84fb73bc36cb7bf26fb7855090a6
Abraham Dulla <abrish.d143t@gmail.com>
2022-11-27 14:56:39 +0300

    swagger setup in servers.js

--- server.js
+++ server.js
@@ -2,7 +2,25 @@ const http = require("http");
 const app = require("./src/api/v1/index");
 const port = 3000;
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDocS = require("swagger-jsdoc");
+
 const server = http.createServer(app);
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Abrish Swagger",
+      description: "Swagger Test Description",
+      version: "1.0",
+    },
+  },
+  apis: ["server.js"],
+};
+
+const swaggerDocs = swaggerJsDocS(swaggerOptions);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 server.listen(port, () => {
   console.log(`Express Crud is running on port http://localhost:${port}`);
