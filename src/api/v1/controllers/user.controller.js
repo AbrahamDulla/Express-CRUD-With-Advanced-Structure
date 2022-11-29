57ab725de3737ebbf5a7b25856bd9841538b4cdd
Abraham Dulla <abrish.d143t@gmail.com>
2022-11-29 13:59:39 +0300

    user CRUD finished

--- src/api/v1/controllers/user.controller.js
+++ src/api/v1/controllers/user.controller.js
@@ -5,8 +5,40 @@ const { request } = require("express");
 const prisma = new PrismaClient();
 
 const allUsers = asyncHandler(async (req, res) => {
-  const users = await prisma.users.findMany();
-  res.json(users);
+  try {
+    const users = await prisma.users.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({
+      error: error,
+      message: error.code,
+    });
+  }
+});
+
+const oneUser = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const users = await prisma.users.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (users) {
+      return res.status(201).json({
+        success: true,
+        status: 201,
+        message: `${users.name} find successfully!!!`,
+        data: users,
+      });
+    }
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({
+      error: error,
+      message: error.code,
+    });
+  }
 });
 
 /**@description create user
@@ -45,17 +77,54 @@ const createUser = asyncHandler(async (req, res) => {
 
 const updateUser = asyncHandler(async (req, res) => {
   try {
-    let user = req.user;
-  } catch (error) {}
+    const { id } = req.params;
+    let { name, email, password } = req.body;
+    const user = await prisma.users.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+    });
+    if (user) {
+      return res.status(201).json({
+        success: true,
+        status: 201,
+        message: "User updated successfully!!!",
+        data: user,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      error: error,
+      message: error.code,
+    });
+  }
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
-  try {
-  } catch (error) {}
+  const { id } = req.params;
+  const user = await prisma.users.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  if (user) {
+    return res.status(201).json({
+      success: true,
+      status: 201,
+      message: "User deleted successfully!!!",
+      data: user,
+    });
+  }
 });
 
 module.exports = {
   createUser,
+  oneUser,
   allUsers,
   updateUser,
   deleteUser,
--- src/api/v1/routes/users.routes.js
+++ src/api/v1/routes/users.routes.js
@@ -2,7 +2,13 @@ const express = require("express");
 // const connection = require("../connection");
 const userRoute = express.Router();
 
-const { createUser, allUsers } = require("../controllers/user.controller");
+const {
+  createUser,
+  allUsers,
+  oneUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user.controller");
 
 /**
  * @swagger
@@ -11,8 +17,11 @@ const { createUser, allUsers } = require("../controllers/user.controller");
  *     summary: Retrieve a list of JSONPlaceholder users.
  *     description: Retrieve a list of users from JSONPlaceholder.
  */
-userRoute.get("/all-users", allUsers);
 userRoute.post("/create", createUser);
+userRoute.get("/all-users", allUsers);
+userRoute.get("/one-user/:id", oneUser);
+userRoute.put("/update/:id", updateUser);
+userRoute.delete("/delete/:id", deleteUser);
 
 userRoute.get("/read", (req, res) => {
   const baseUrl = req.baseUrl;
